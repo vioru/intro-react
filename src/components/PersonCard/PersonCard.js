ebefd50ccfc6285281d7be1dfff8653951c608a2
Fernanda Villalba <78764267+vioru@users.noreply.github.com>
2022-09-30 21:41:56 -0500

    boton cumpleaños

--- src/components/PersonCard/PersonCard.js
+++ src/components/PersonCard/PersonCard.js
@@ -2,16 +2,29 @@ import React from "react";
 
 
 class PersonCard extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    cumpleaños = () => {
+        this.setState({age:  parseInt(this.state.age)+1})
+    } 
+
     render(){
 
         const {FirstName , LastName , age, HairColor, logo} = this.props
         return(
-            <div className="card col-10 my-4 ">
+            <div className="card col-10 my-4 border-info ">
                 <div className="row align-items-center my-2 ">
                     <div className="col-8 ">
                         <h2>{LastName}, {FirstName} </h2>
-                        <p><b>Edad:</b>{age}</p>
+                        <p><b>Edad:</b>{this.state.age}</p>
                         <p><b>Color de Cabello:</b>{HairColor}</p>
+                        <button className="btn btn-info" onClick={this.cumpleaños}>Botón de Cumpleaños {FirstName}</button>
                     
                     </div>
 
